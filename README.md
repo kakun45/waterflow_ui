39d3ac04ac2a7312bbb31a2993ca9ddf2b1c0ee8
kakun45 <email4blackkat@gmail.com>
2023-01-02 17:20:32 -0500

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # waterflow_ui
 User interface for a DAG execution engine
+
+VSCode LiveServer is listening on http://127.0.0.1:5500/index.html
+
+command to run from local folder: 
+`python3 -m http.server`
+listening on http://0.0.0.0:8000/
---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("hi"); // works
+
+// http://localhost/ui/stats/jobs
+
+// {"pending_count": 643, "fetching_count": 5, "running_count": 20, "succeeded": 0, "failed": 0}
+
+
+// http://localhost/ui/stats/tasks
+
+// {"blocked_count": 3640, "pending_count": 0, "paused_count": 0, "running_count": 0, "succeeded_count": 0, "failed_count": 0}
